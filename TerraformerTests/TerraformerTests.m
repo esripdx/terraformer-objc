cd9ec52c95f7cde2a85f9ddfb2dad5273af111d9
Ryan Arana <ryan@ryanarana.com>
2014-05-21 16:34:37 -0700

    get rid of merge artifact

--- TerraformerTests/TerraformerTests.m
+++ TerraformerTests/TerraformerTests.m
@@ -28,42 +28,4 @@
     [super tearDown];
 }
 
-- (void)testCoordinate {
-    TFCoordinate *c = [TFCoordinate coordinateWithX:5 y:10];
-    XCTAssertEqual(5, c.x);
-    XCTAssertEqual(10, c.y);
-}
-
-- (void)testCoordinateCopy;
-{
-    TFCoordinate *original = [TFCoordinate coordinateWithX:5.0 y:10.1];
-    TFCoordinate *copy = [original copy];
-    TFCoordinate *other = [TFCoordinate coordinateWithX:1.0 y:11.1];
-    
-    XCTAssertTrue( [original isEqual:copy] );
-    XCTAssertEqual( original.x, copy.x );
-    XCTAssertEqual( original.y, copy.y );
-    XCTAssertFalse( [original isEqual:other] );
-}
-
-- (void)testGeometryBbox {
-    // TODO: Add more types here, and use their specific subclasses.
-
-    TFGeometry *point = [[TFGeometry alloc] initWithType:@"Point" coordinates:@[[TFCoordinate coordinateWithX:5 y:10]]];
-    NSArray *bbox = [point bbox];
-    NSArray *expected = @[@(5), @(10), @(5), @(10)];
-    XCTAssertEqualObjects(expected, bbox);
-
-    TFGeometry *polygon = [[TFGeometry alloc] initWithType:@"Polygon" coordinates:@[
-            [TFCoordinate coordinateWithX:-5 y:-10],
-            [TFCoordinate coordinateWithX:-2 y:-40],
-            [TFCoordinate coordinateWithX:0 y:35],
-            [TFCoordinate coordinateWithX:5 y:10],
-            [TFCoordinate coordinateWithX:25 y:5]
-    ]];
-    bbox = [polygon bbox];
-    expected = @[@(-5), @(-40), @(25), @(35)];
-    XCTAssertEqualObjects(expected, bbox);
-}
-
 @end
