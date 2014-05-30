b4710c66f3c8549a91615aa3d5ad535e2fd3352a
Marc Charbonneau <marc@mbcharbonneau.com>
2014-05-30 16:34:31 -0700

    Polygon contains string stub.

--- Terraformer/TFPolygon.m
+++ Terraformer/TFPolygon.m
@@ -11,10 +11,12 @@
 #import "TFCoordinate.h"
 #import "TFMultiPolygon.h"
 #import "TFPoint.h"
+#import "TFLineString.h"
 
 @interface TFPolygon()
 
 - (BOOL)containsPoint:(TFPoint *)point;
+- (BOOL)containsLine:(TFLineString *)line;
 - (BOOL)polygonWithin:(TFPolygon *)polygon;
 - (BOOL)multiPolygonWithin:(TFMultiPolygon *)multiPolygon;
 
@@ -203,6 +205,44 @@
     return contains;
 }
 
+- (BOOL)containsLine:(TFLineString *)line;
+{
+    NSInteger count = [line.coordinates count];
+    BOOL contains = ( count == 0 ) ? NO : [self containsPoint:[TFPoint pointWithCoordinate:line.coordinates[0]]];
+
+    if ( !contains ) {
+        return NO;
+    }
+    
+    if ( count == 1 ) {
+        return contains;
+    }
+    
+    // For each coordinate pair that makes up a line in the line string, test
+    // it to see if it intersects a line within the polygon. If so, the line is
+    // outside of the polygon.
+    
+    NSArray *outerRing = self.coordinates[0];
+    NSInteger i, j, nvert = [outerRing count];
+    
+    for ( i = 0, j = nvert - 1; i < nvert; j = i++ ) {
+        
+        TFCoordinate *a = outerRing[i];
+        TFCoordinate *b = outerRing[j];
+        
+        // Check holes as well.
+        
+        for ( i = 0; i < [self numberOfHoles]; i++ ) {
+            TFPolygon *polygon = [self holeAtIndex:i];
+            
+            if ( [polygon containsL])
+        }
+
+    }
+    
+    return YES;
+}
+
 - (BOOL)polygonWithin:(TFPolygon *)polygon;
 {
     if ( [self isEqualToPolygon:polygon] ) {
@@ -246,6 +286,9 @@
     switch ( geometry.type ) {
         case TFPrimitiveTypePoint:
             contains = [self containsPoint:(TFPoint *)geometry];
+            break;
+        case TFPrimitiveTypeLineString:
+            contains = [self containsLine:(TFLineString *)geometry];
             break;
         default:
             NSAssert( NO, @"unhandled type" );
