edf05036ef4799845db15b15f2240e81f5090870
Ryan Arana <ryan@ryanarana.com>
2014-06-30 17:29:39 -0700

    Fix a bug in isClockwise implementation which caused isClockwise to falsely report true if the differences between the coordinates were < 1 (use a double instead of an int)

--- Terraformer/Formats/TFEsriJSON.m
+++ Terraformer/Formats/TFEsriJSON.m
@@ -387,9 +387,11 @@ static NSString *const TFAttributesKey = @"attributes";
 
 @implementation TFLineString (esrijson)
 
-/** Determines whether or not the direction of the coordinates in a ring is clockwise. */
+/** Determines whether or not the direction of the coordinates in a ring is clockwise using the Shoelace Formula.
+* http://en.wikipedia.org/wiki/Shoelace_formula
+**/
 - (BOOL)isClockwise {
-    NSInteger total = 0;
+    double total = 0;
     TFPoint *p1, *p2;
     p1 = self[0];
     for (NSUInteger i = 0; i < self.count - 1; i++) {
